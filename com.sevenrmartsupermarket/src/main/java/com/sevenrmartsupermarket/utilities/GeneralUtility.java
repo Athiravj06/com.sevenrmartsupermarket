4f42c42138717a2a296d1c0fce82bb70d3ac1850
Athira <Athira@192.168.1.3>
2024-10-02 19:11:01 +0530

    final

--- com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/utilities/GeneralUtility.java
+++ com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/utilities/GeneralUtility.java
@@ -17,20 +17,12 @@ public class GeneralUtility {
 	public String getCssValue(WebElement element, String CssValue) {
 		return element.getCssValue(CssValue);
 	}
-	 //to return data from list of webelements
-
 	 public List<String>get_textofelements(List<WebElement> elements){
-
 		 List<String> data=new ArrayList<String>();
-
 		 for(WebElement element : elements) {
-
 			 data.add(element.getText());		 
-
 		 }
-
-		 return data;
-		 
+		 return data;	 
 	 }
 	 
 	 public static String getRandomFirstName() {
